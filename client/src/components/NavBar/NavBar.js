508648703034aaee1919d003501d31b91d150d31
Sina <sina202@gmail.com>
2020-02-03 16:39:31 -0800

    add conditional to not show share button in nav bar on the share page

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -11,9 +11,24 @@ import {
 } from "@material-ui/core/";
 import logo from "../../images/boomtown.svg";
 import { Add, MoreVert } from "@material-ui/icons/";
+import { withRouter } from "react-router-dom";
 import styles from "./styles";
 
-const NavBar = ({ classes }) => {
+const options = ["Profile", "Sign"];
+
+const NavBar = ({ classes, location }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  console.log(location);
   return (
     <div>
       <AppBar position="fixed">
@@ -22,21 +37,46 @@ const NavBar = ({ classes }) => {
             <img src={logo} className={classes.logo} />
           </Link>
           <div>
-            <Button
-              size="large"
-              color="secondary"
-              className={classes.navButton}
-            >
-              <Add className={classes.addIcon} />
-              Share Something
-            </Button>
+            {location.pathname !== "/share" ? (
+              <Button
+                size="large"
+                color="secondary"
+                className={classes.navButton}
+              >
+                <Add className={classes.addIcon} />
+                Share Something
+              </Button>
+            ) : null}
             <IconButton
               aria-label="more"
               aria-controls="long-menu"
               aria-haspopup="true"
+              onClick={handleClick}
             >
               <MoreVert />
             </IconButton>
+            <Menu
+              id="long-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  width: 200
+                }
+              }}
+            >
+              {options.map(option => (
+                <MenuItem
+                  key={option}
+                  selected={option === "Pyxis"}
+                  onClick={handleClose}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
           </div>
         </Toolbar>
       </AppBar>
@@ -44,4 +84,4 @@ const NavBar = ({ classes }) => {
   );
 };
 
-export default withStyles(styles)(NavBar);
+export default withRouter(withStyles(styles)(NavBar));
